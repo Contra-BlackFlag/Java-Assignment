47cb0d6b179ec8ede6057b1c6bad9d58d3d50bb9
Contra-BlackFlag <pvvishwa12345@gmail.com>
2026-09-02 20:13:31 +0530

    model

---
+++ src/main/java/com/contra/javaassignment/Model/Wallet.java
@@ -0,0 +1,30 @@
+package com.contra.javaassignment.Model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "wallets")
+public class Wallet {
+
+    @Id
+    private UUID userId;
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    public Wallet() {}
+    public Wallet(UUID userId, BigDecimal balance) {
+        this.userId = userId;
+        this.balance = balance;
+    }
+
+    public UUID getUserId() { return userId; }
+    public BigDecimal getBalance() { return balance; }
+    public void setBalance(BigDecimal balance) { this.balance = balance; }
+}
